14feb72e3846650ade8db129c4ff9f00065dd616
Octagon-simon <ugorji757@gmail.com>
2021-08-30 04:12:20 -0700

    Added 5th script file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*Using Puts function for standard output*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
