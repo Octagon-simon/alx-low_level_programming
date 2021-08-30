573ab8521671bac5bd5c04b1166efa8cfd2e35c5
Octagon-simon <ugorji757@gmail.com>
2021-08-30 12:43:53 -0700

    Updatd 2nd script file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,10 @@ lastDigit = n % 10;
 if (lastDigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-}
- 
+} 
 if (lastDigit == 0)
 {
-printf("Last digit of %d is %d and is zero\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 if (lastDigit < 6 && lastDigit != 0)
 {
