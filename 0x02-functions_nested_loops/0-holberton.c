fa647b94e58122395928f6293022875595527ce4
Octagon-simon <ugorji757@gmail.com>
2021-08-31 13:25:04 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Print all possible combinations of single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the very begining of the project 0x02-functions_nested_loops
