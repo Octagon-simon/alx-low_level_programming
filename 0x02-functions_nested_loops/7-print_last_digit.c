72d203aff0c15ce2ceab123990956acbb858ca9a
Octagon-simon <ugorji757@gmail.com>
2021-09-01 08:57:01 -0700

    int print_last_digit(int)

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,16 @@
 */
 int print_last_digit(int n)
 {
-int r;
-r = n % 10;
-if (n < 0)
+int l;
+l = n % 10;
+if (l < 0)
 {
-return (-r + 0);
+_putchar(-l + 48);
+return (-l);
 }
 else
 {
-return (r + 0);
+_putchar(l + 48);
+return (l);
 }
 }
