c527ba3b12ffa5ee39011a31779bf92286008ca2
Octagon-simon <ugorji757@gmail.com>
2021-09-01 01:28:07 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 int i;
-char c[]= "_putchar";
-for(i = 0; i <8; i++)
+char c[] = "_putchar";
+for (i = 0; i <8; i++)
 {
 _putchar(c[i]);
 }
 _putchar('\n');
-return(0);
+return (0);
 }
