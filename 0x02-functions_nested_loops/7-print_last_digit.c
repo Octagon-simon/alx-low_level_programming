ced8ac79daf85729ae53fbdab7a4abfa709d6b9a
Octagon-simon <ugorji757@gmail.com>
2021-09-01 08:51:38 -0700

    int print_last_digit(int)

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,20 @@
 /**
 * print_last_digit - prints the last digit of a number.
 *
-*
+* @n: The integer to check
 *
 * Return: Always 0
 */
 int print_last_digit(int n)
 {
 int r;
+if (n < 0)
+{
 r = n % 10;
-return (r);
+return (-r + 0);
+}
+else
+{
+return (r + 0);
+}
 }
