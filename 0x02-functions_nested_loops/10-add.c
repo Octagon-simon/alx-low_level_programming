78959f3ade5274976f5b5599527143ab920f9ae0
Octagon-simon <ugorji757@gmail.com>
2021-09-01 13:56:42 -0700

    int add(int, int)

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,14 @@
 #include "main.h"
 /**
 * add - Adds two integers
-* 
+*
 * @x: The first integer
 *
 * @y: The second integer
 *
 * Return: Always 0.
 */
-int add (int x, int y)
+int add(int x, int y)
 {
 int r;
 r = x + y;
