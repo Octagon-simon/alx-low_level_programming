77de74c0afa83bb5f3da75be23127f6e893e8a00
Octagon-simon <ugorji757@gmail.com>
2021-09-01 01:51:19 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void)
+void print_alphabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-  print_alphabet();
   _putchar(ch);
 }
 _putchar('\n');
