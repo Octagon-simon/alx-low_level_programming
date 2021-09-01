1fc3e5dbd48c1405a1c6501fd626cdaae7f2d792
Octagon-simon <ugorji757@gmail.com>
2021-09-01 06:59:03 -0700

    int print_sign(int n)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - Checks number sign
+*
+* int n - Function Argument and type
+*
+* @n: The number to check
+*
+* Return: 1 if n > 0, or 0 if n = 0, or -1 if otherwise
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar("+");
+return (1);
+}
+if (n = 0)
+{
+_putchar("0");
+return (0);
+}
+if (n < 0)
+{
+_putchar("-1");
+return (-1);
+}
+}
