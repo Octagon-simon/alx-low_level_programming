34ae7aa935de4a666fd18bc086522980ba13e293
Octagon-simon <ugorji757@gmail.com>
2021-09-07 00:56:31 -0700

    int _strlen(char *s)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *
+* _strlen - returns the length of a string..
+* @s: The value to evaluate.
+* Return: Length of string
+*
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+s++;
+l++;
+}
+return (l);
+}
