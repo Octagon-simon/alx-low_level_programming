104ec7f6a239eb53bf1e7fd37aa43eacd11f23f9
Octagon-simon <ugorji757@gmail.com>
2021-09-07 07:32:45 -0700

    void print_array(int *a, int n)

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_array - function that prints n elements of an array of integers, followed by a new line
+ *@a: value to be evaluate.
+ *@n: value to be evaluate.
+ *Return: none.
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0 ; x < n; x++)
+	{
+		printf("%d", a[x]);
+		if (x != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
