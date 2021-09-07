6a008fecadd83958af02d5d0487eeeccd9376003
Octagon-simon <ugorji757@gmail.com>
2021-09-07 07:29:02 -0700

    void puts_half(char *str)

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *puts_half - function that prints half of a string, followed by a new lin.
+ *@str: value to be evaluate.
+ *Return: none
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	char *y = str;
+	int n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for ( ; n < len ; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
