9e8a5beeefbf3605954ac5de1a34831e2d6e6544
Octagon-simon <ugorji757@gmail.com>
2021-09-27 05:20:46 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**                                                             
+* main -return file name
+*
+* Return: Always 0
+**/
+int main(void){
+printf(__FILE__);
+return(0);
+}
