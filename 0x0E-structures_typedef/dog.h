23be7a76fd32bbf0c3f9a1d853a1647e86317d45
Octagon-simon <ugorji757@gmail.com>
2021-09-28 17:42:18 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG
 #define DOG
+/**
+*struct dog - defines a structure dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*Return: Always 0
+**/
 struct dog
 {
 char *name;
