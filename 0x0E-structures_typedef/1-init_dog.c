90d3294bf783583b5024762333bfb8ef63610035
Octagon-simon <ugorji757@gmail.com>
2021-09-28 18:11:53 -0700

    1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 /**
 *init_dog - function prototype
 *@d: poiner
@@ -14,4 +15,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
+int main(void)
+{
+struct dog my_dog;
+init_dog(&my_dog, "Poppy", 3.5, "Bob");
+printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+return (0);
 }
