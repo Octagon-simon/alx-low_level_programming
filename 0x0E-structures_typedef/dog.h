4d5ba54f52dcbecab0a23d794e59fcbce2ee9052
Octagon-simon <ugorji757@gmail.com>
2021-09-28 19:12:32 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,10 +7,13 @@
 *@owner: owner of dog
 *Return: Always 0
 **/
-struct dog_t
+struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
 #endif
