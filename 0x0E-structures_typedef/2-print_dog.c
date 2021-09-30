57d096d793aa10110fe0dd18c604ecbddacd46c6
Octagon-simon <ugorji757@gmail.com>
2021-09-30 07:55:48 -0700

    Updated 2=print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,14 +16,14 @@ if (d->name != NULL)
 printf("Name: %s\n", d->name);
 else
 {
-printf("Name: (nill)\n");
+printf("Name: (nil)\n");
 }
 printf("Age: %f\n", d->age);
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
 {
-printf("Owner: (nill)\n");
+printf("Owner: (nil)\n");
 }
 }
 }
