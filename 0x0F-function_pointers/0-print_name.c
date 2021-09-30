4e6bb18acc589d04f6fd5bfb4f37544391690df5
Octagon-simon <ugorji757@gmail.com>
2021-09-30 13:55:44 -0700

    Added some scripts

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include"stdlib.h"
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - prints a name
- * @name: name of the being
+ * @name: pointer to char
  * @f: pointer to function that returns nothing
- * Return: Nothing.
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL || *name != '\0' || f != 'NULL')
-f(name);
+	if (name != NULL && *name != '\0' && f != NULL)
+	f(name);
 }
-
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * array_iterator - executes a given function
- * @array: array
- * @size: length of array
- * @action: pointer to function that returns nothing
- * Return: Nothing.
+ * array_iterator - function executes a function on array
+ * @array: pointer to array
+ * @size: number elements in array
+ * @action: pointer to function
+ * Return: nothing
  **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
-for (i = 0; i < size; i++)
-if (array != NULL)
-{
-action(array[i]);
-}
+	unsigned int i;
+
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
