441e3e5c8a8a88823f349127219c5d8388e368a4
Octagon-simon <ugorji757@gmail.com>
2021-09-30 18:44:54 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - writes the character c to stdout
+ * @separator: The character used for seperation
+ * @n: The numbers to print
+ * Return: nothing
+ * 
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+if (separator != NULL)
+ {
+va_list nums;
+unsigned int i;
+int result;
+va_start(nums, n);
+for (i = 0; i < n; i++){ 
+result = va_arg(nums, int);
+printf("%d", result);
+/*Use this block to check if the last element is reached*/
+if (i < (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+va_end(nums);
+}
+}
