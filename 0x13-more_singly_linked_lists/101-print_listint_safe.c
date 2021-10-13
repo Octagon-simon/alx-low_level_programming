c8fe1059a97ecdad879b177292d92531eafd4777
Octagon-simon <ugorji757@gmail.com>
2021-10-13 13:40:56 -0700

    Updated some scripts

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_listint_safe - define function
- * @head:
+ * @head: node
  * Return: what does it return?
  */
 size_t print_listint_safe(const listint_t *head)
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
  * find_listint_loop - function that finds the loop in a linked list
- * @head:
+ * @head: node
  * Return: return loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-  head++;
-  return (0);
+head++;
+return (0);
 }
